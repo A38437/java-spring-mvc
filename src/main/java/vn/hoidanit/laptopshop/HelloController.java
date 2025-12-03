3ae87b7dfacd7e72a1c0d0ee6ec56c746a750160
JustST <lythien632002@gmail.com>
2025-12-03 10:31:30 +0700

    update db

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,6 +8,16 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World with Thien!";
+        return "Hello World!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
     }
 }
