a44520eda41fab681db55aa80500c70e831e16dc
JustST <lythien632002@gmail.com>
2025-12-08 23:29:25 +0700

    update ADatabase

---
+++ src/main/java/vn/hoidanit/laptopshop/domain/Role.java
@@ -0,0 +1,49 @@
+package vn.hoidanit.laptopshop.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+
+    private String name;
+
+    private String description;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [Id=" + Id + ", name=" + name + ", description=" + description + "]";
+    }
+
+}
